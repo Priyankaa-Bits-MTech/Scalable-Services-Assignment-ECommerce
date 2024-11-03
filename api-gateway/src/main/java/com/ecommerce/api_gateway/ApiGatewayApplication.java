ef75e9d4740faa98e4c756e6a1bf019a0f4babda
Anantha Krishnan <anantha.krishnan@outlook.com>
2024-11-03 18:47:30 +0530

    Add Gateway

---
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.ecommerce.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+    WebProperties.Resources resources() {
+       return new WebProperties.Resources();
+   }
+
+
+}
