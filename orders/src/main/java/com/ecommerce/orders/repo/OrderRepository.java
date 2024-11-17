4c86adfa96f764a4c05be62fb05111f32bf80868
Ishan Vohra <ishanv@powrofyou.com>
2024-11-17 13:56:52 +0530

    chore: Remove unused import statement

--- orders/src/main/java/com/ecommerce/orders/repo/OrderRepository.java
+++ orders/src/main/java/com/ecommerce/orders/repo/OrderRepository.java
@@ -3,8 +3,6 @@ package com.ecommerce.orders.repo;
 import com.ecommerce.orders.entity.OrderData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ecommerce.orders.entity.Cart;
-
 
 public interface OrderRepository extends  JpaRepository<OrderData, Integer> {
 
